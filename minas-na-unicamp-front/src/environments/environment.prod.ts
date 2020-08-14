1b13b4a0dd24e515f45eba1acbf8a9eb24ac6d15
Barbara Karoline Perim <barbarap@ciandt.com>
2020-08-14 00:21:47 -0300

    deploy configs

--- minas-na-unicamp-front/src/environments/environment.prod.ts
+++ minas-na-unicamp-front/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAFqfEpfhlZSKXvy3Pfp30gTvwwxtJvttM',
+    authDomain: 'minas-na-unicamp.firebaseapp.com',
+    databaseURL: 'https://minas-na-unicamp.firebaseio.com',
+    projectId: 'minas-na-unicamp',
+    storageBucket: 'minas-na-unicamp.appspot.com',
+    messagingSenderId: '414837139991',
+    appId: '1:414837139991:web:0adc1dab882a7b430f8a52',
+    measurementId: 'G-K01CXN3V2Y',
+  }
 };
